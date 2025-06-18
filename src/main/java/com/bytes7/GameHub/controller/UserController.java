b5d8d066a65800ba82cb171f21800bc4c324a929
Ricardo Requena <rjrequena47@gmail.com>
2025-06-18 19:41:29 -0400

    Endpoint /api/users/me implementado para retorno de usuario autenticado

---
+++ src/main/java/com/bytes7/GameHub/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bytes7.GameHub.controller;
+
+import com.bytes7.GameHub.dto.response.UserResponse;
+import com.bytes7.GameHub.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/me")
+    @PreAuthorize("hasAnyRole('PLAYER', 'ADMIN')")
+    public ResponseEntity<UserResponse> getCurrentUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+}
