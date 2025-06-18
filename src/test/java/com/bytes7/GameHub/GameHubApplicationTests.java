8e0c58216b5c573be94c69dd47ef50c65009dcc8
Ricardo Requena <rjrequena47@gmail.com>
2025-06-17 22:18:58 -0400

    Primer commit a repositorio remoto

---
+++ src/test/java/com/bytes7/GameHub/GameHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytes7.GameHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
